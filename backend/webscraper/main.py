549f89cf3ad04a2950bb5340af9573cf8b8dc905
tobiashungwe <tobias.hungwe@student.howest.be>
2024-10-26 19:46:20 +0200

    add dom content fitlering and cleaning using beautifullsoup

--- backend/webscraper/main.py
+++ backend/webscraper/main.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from scrape import scrape
-
+from scrape import scrape, split_dom_content, clean_content, extract_content
 # Set the title of the app
 st.title('Machine Engine')
 
@@ -14,5 +13,19 @@ url = st.text_input('Enter the URL of the website you want to scrape')
 if st.button("Scrape Site"):
     st.write("Scraping the website")
     result = scrape(url)
-    print(result)
+    body_content = extract_content(result)
+    cleaned_content = clean_content(body_content)
+    st.session_state.dom_content = cleaned_content
+    
+    with st.expander("View Dom Content"):
+        st.text_area("Dom Content", cleaned_content, height=300)
 
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse? ")
+    
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing content")
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            
---
+++ backend/webscraper/parse.py
@@ -0,0 +1,3 @@
+from langchain_ollama import OllamaLLM 
+from langchain_core.prompts import ChatPromptTemplate
+
